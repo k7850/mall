033ecf5b7601d309858b7d8cf4747d02fb0a7309
k7850 <k79357850@gmail.com>
2023-07-18 16:33:55 +0900

    시작

---
+++ src/main/java/shop/metacoding/mall/controller/ProductController.java
@@ -0,0 +1,19 @@
+package shop.metacoding.mall.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ProductController {
+
+    @GetMapping("/")
+    public String home(){
+        return "home";
+    }
+
+    @GetMapping("/write")
+    public String write(){
+        return "write";
+    }
+}
+
