361b4f43604c4bc840084eda8396cdae123214c1
k7850 <k79357850@gmail.com>
2023-07-20 16:51:09 +0900

    삭제 수정 추가

--- src/main/java/shop/metacoding/mall/controller/ProductController.java
+++ src/main/java/shop/metacoding/mall/controller/ProductController.java
@@ -3,6 +3,7 @@ package shop.metacoding.mall.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import shop.metacoding.mall.model.Product;
 import shop.metacoding.mall.model.ProductRepository;
@@ -18,6 +19,34 @@ public class ProductController {
     @Autowired
     private ProductRepository productRepository;
 
+
+    @PostMapping("/product/update")
+    public String update(int id, String name, int price, int qty) {
+        productRepository.update(id, name, price, qty);
+        return "redirect:/product/"+id;
+    }
+
+
+    @PostMapping("/product/delete")
+    public String delete(int id) {
+        productRepository.deleteById(id);
+        return "redirect:/"; // 리다이렉트 하지 않고 그냥  return "home";  을 하면 상품목록이 안나온다. 리퀘스트에 안담았으니까
+//        response.sendRedirect("/"); // 이거로도 되지만  매개변수에 HttpServletResponse response 넣고 반환타입을 void로 바꿔야함
+    }
+
+
+
+    @GetMapping("/product/{id}")
+    public String detail(@PathVariable int id, HttpServletRequest request){
+        System.out.println("id: "+id);
+        Product product = productRepository.findById(id);
+        request.setAttribute("p", product);
+
+        return "detail";
+    }
+
+
+
     @GetMapping("/")
     public String home(HttpServletRequest request){
         List<Product> productList = productRepository.findAll();
@@ -31,15 +60,14 @@ public class ProductController {
     }
 
     @PostMapping ("/product")
-    public void write(String name, int price, int qty, HttpServletResponse response) throws IOException {
+    public String write(String name, int price, int qty) throws IOException {
         System.out.println("name :"+name);
         System.out.println("price :"+price);
         System.out.println("qty :"+qty);
 //        return "write";
-
         productRepository.save(name, price, qty);
-        response.sendRedirect("/");
-//        return "redirect:/";
+//        response.sendRedirect("/");
+        return "redirect:/";
     }
 
 }
--- src/main/java/shop/metacoding/mall/model/ProductRepository.java
+++ src/main/java/shop/metacoding/mall/model/ProductRepository.java
@@ -24,8 +24,56 @@ public class ProductRepository {
     }
 
     public List<Product> findAll() {
-       Query query = em.createNativeQuery("select * from product_tb", Product.class);
-       List<Product> productlist = query.getResultList();
-       return productlist;
+        Query query = em.createNativeQuery("select * from product_tb", Product.class);
+        List<Product> productlist = query.getResultList();
+        return productlist;
     }
+
+    public Product findById(int id) {
+        Query query = em.createNativeQuery("select * from product_tb where id = :id", Product.class);
+        query.setParameter("id", id);
+        Product product = (Product) query.getSingleResult();
+        return product;
+    }
+
+    public Product findById2(int id) {
+        Query query = em.createNativeQuery("select * from product_tb where id = :id");
+        query.setParameter("id", id);
+        // row가 1건
+        // 1, 바나나, 1000, 50
+
+        Object[] object = (Object[]) query.getSingleResult();
+        int id2 = (int) object[0];
+        String name2 = (String) object[1];
+        int price2 = (int) object[2];
+        int qty2 = (int) object[3];
+
+        Product product = new Product();
+        product.setId(id2);
+        product.setName(name2);
+        product.setPrice(price2);
+        product.setQty(qty2);
+        return product;
+    }
+
+    @Transactional // db작업 전부 정상적으로 종료되면 커밋, 예외가 발생하면 롤백   /   DB 변경(C,U,D) 있는데 @Transactional 빼먹으면 오류뜸
+    public void deleteById(int id) {
+        Query query = em.createNativeQuery("delete from product_tb where id=:id");
+        query.setParameter("id", id);
+        query.executeUpdate();
+    }
+
+
+    @Transactional // db작업 전부 정상적으로 종료되면 커밋, 예외가 발생하면 롤백   /   DB 변경(C,U,D) 있는데 @Transactional 빼먹으면 오류뜸
+    public void update(int id, String name, int price, int qty) {
+        Query query = em.createNativeQuery("update product_tb set name=:name, price=:price, qty=:qty where id=:id");
+        query.setParameter("id", id);
+        query.setParameter("name", name);
+        query.setParameter("price", price);
+        query.setParameter("qty", qty);
+        query.executeUpdate();
+    }
+
+
 }
+
